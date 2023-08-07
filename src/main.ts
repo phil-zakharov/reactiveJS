c5b2d7a8237005cbaa1ef0b51f071005cb738b14
Philipp Zakharov <philippzakharov@gmail.com>
2023-08-07 19:37:17 +0300

    config ts

--- src/main.ts
+++ src/main.ts
@@ -50,34 +50,3 @@ async function main() {
 
 main();
 
-// function createEmitter(value: number) {
-//   const instance = new Emitter<number>(value);
-
-//   return instance;
-// }
-
-// function createHandler(...emitter: Emitter<number>[] | Handler<number>[]) {
-//   const instance = new Handler(emitter);
-
-//   return instance;
-// }
-
-// const a = createEmitter(1),
-//   b = createEmitter(2),
-//   c = createHandler(a, b),
-//   d = createHandler(c).map((aValue: any, bValue: any) => aValue + bValue + 5);
-
-// console.log('1 -> ', a.getValue());
-// console.log('2 -> ', b.getValue());
-// console.log('[1,2] -> ', c.getValue());
-// console.log('8 -> ', d.getValue());
-
-// a.setValue((val) => val as number + 10);
-
-// console.log('11 -> ', a.getValue());
-// console.log('2 -> ', b.getValue());
-// console.log('18 -> ', d.getValue());
-
-// c.map((a, b) => a + b)
-
-// console.log('13 -> ', c.getValue())
